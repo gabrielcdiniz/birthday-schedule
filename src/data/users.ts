43d26e9496ac5de231a23915b8d01a41bd11e686
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2022-05-27 22:52:02 -0300

    feat: add "data"

---
+++ src/data/users.ts
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    id: BigInt("809540925767548978"),
+    name: "Gabriel",
+    birthday: "2022-08-12",
+  },
+  {
+    id: BigInt("809540925767548978"),
+    name: "Dummy",
+    birthday: "2022-05-27",
+  },
+  {
+    id: BigInt("965415778893594626"),
+    name: "Dhyon",
+    birthday: "2001-02-16",
+  },
+  {
+    id: BigInt("227158720642154498"),
+    name: "Dave",
+    birthday: "1996-11-07",
+  },
+  {
+    id: BigInt("273522835081658370"),
+    name: "Marcelo",
+    birthday: "",
+  },
+  {
+    id: BigInt("825849275933917186"),
+    name: "Douglas",
+    birthday: "",
+  },
+  {
+    id: BigInt("653638322609979392"),
+    name: "Verônica",
+    birthday: "",
+  },
+];
